a68ac88c0ec2b9c0cefa6baa5819a5c2c4a3fec3
11StarLord <diogowalter114@gmail.com>
2025-03-31 10:40:09 +0100

    adding process_and_validate_line

--- src/token/token_analysis.c
+++ src/token/token_analysis.c
@@ -1,5 +1,25 @@
 #include "minishell.h"
 
+static void copy_remaining_tokens(t_shell *shell, t_token *tokens, int *i)
+{
+	int	j;
+
+	j = 0;
+	if (tokens[0].str)
+	{
+		shell->tokens[*i].str = ft_strdup("|");
+		shell->tokens[(*i)++].type = "PIPE";
+	}
+	while (tokens[j].str)
+	{
+		shell->tokens[*i].str = ft_strdup(tokens[j].str);
+		shell->tokens[*i].type = tokens[j].type;
+		(*i)++;
+		j++;
+	}
+	shell->tokens[*i].str = NULL;
+}
+
 static void add_heredoc_tokens(t_shell *shell, char *str_heredoc, int *i)
 {
 	shell->tokens[*i].str = ft_strdup("/bin/echo/");
@@ -13,38 +33,26 @@ static void add_heredoc_tokens(t_shell *shell, char *str_heredoc, int *i)
 static void	dup_tokens(t_shell *shell, t_token *tokens, char *str_heredoc)
 {
 	int	i;
-	int	numb_tokens;
 
-	i = 0;
-	numb_tokens = 0;
-	while (tokens[i].str)
-	{
-		numb_tokens++;
-		i++;
-	}
-	shell->tokens = (t_token *)malloc(sizeof(t_token) * (numb_tokens + 1));
-	if (!shell->tokens)
-		return ;
 	i = 0;
 	if (str_heredoc && str_heredoc[0])
 	{
+		shell->tokens = (t_token *)malloc(sizeof(t_token) * (shell->tokens_size + 4));
 		add_heredoc_tokens(shell, str_heredoc, &i);
+		copy_remaining_tokens(shell, tokens, &i);
+	}
+	else
+	{
+		shell->tokens = (t_token *)malloc(sizeof(t_token) * (shell->tokens_size + 1));
+		while (tokens[i].str)
+		{
+			shell->tokens[i].str = ft_strdup(tokens[i].str);
+			shell->tokens[i].type = tokens[i].type;
+			i++;
+		}
+		shell->tokens[i].str = NULL;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 static void process_and_validate_line(t_shell *shell, t_token **tokens)
 {
@@ -85,12 +93,12 @@ void	token_analysis(t_shell *shell, char *input_line)
 		ft_free(input_line);
 		return ;
 	}
-	shell->charge = 1;
+	/*shell->charge = 1;
 	int test =handle_redirection(shell, 0, 0);
 	if(test == 1)
 		printf("Success\n");
 	else
 		printf("Failed\n");
-	/*ft_free_tokens(tokens);
+	ft_free_tokens(tokens);
 	ft_free(input_line);*/
 }

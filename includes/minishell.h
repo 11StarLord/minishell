ca19b606e268e5c953b20c963d5b52061c627bff
11StarLord <diogowalter114@gmail.com>
2025-04-02 00:10:11 +0100

    updating minishell.h adding dup_tokens

--- includes/minishell.h
+++ includes/minishell.h
@@ -88,6 +88,5 @@ int	create_pipe_process(t_shell *shell);
 bool is_valid_redirect_syntax(t_token *tokens, t_shell *shell);
 void	dup_tokens(t_shell *shell, t_token *tokens);
 int	handle_redirection(t_shell *shell, int token_index, int is_pipe);
-char	*handle_heredoc(t_shell *shell, t_token *tokens, char **str_heredoc);
 
 #endif

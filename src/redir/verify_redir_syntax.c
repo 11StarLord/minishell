cde8fcb705f77b4e07c7fa605c785453ee240458
Diogo Joao <djoao@c1r3s7.42luanda.com>
2025-04-01 14:33:37 +0100

    adding verify_redir_syntax

---
+++ src/redir/verify_redir_syntax.c
@@ -0,0 +1,46 @@
+#include "minishell.h"
+
+static bool is_valid_previous_token(t_token *tokens, int i)
+{
+	if (tokens[i - 1].str)
+	{
+		if (is_type_token(tokens[i - 1], "ARGUMENT"))
+			return (true);
+	}
+	ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+	ft_putstr_fd(tokens[i].str, 2);
+    ft_putstr_fd("'\n", 2);
+	return (false);
+}
+
+static bool is_valid_next_token(t_token *tokens, int i)
+{
+	if (tokens[i + 1].str)
+	{
+		if (is_type_token(tokens[i + 1], "ARGUMENT"))
+			return (true);
+	}
+	ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+	ft_putstr_fd(tokens[i].str, 2);
+    ft_putstr_fd("'\n", 2);
+	return (false);
+}
+
+bool is_valid_redirect_syntax(t_token *tokens)
+{
+	int	i;
+
+	i = 0;
+	while (tokens[i].str)
+	{
+		if (is_type_token(tokens[i], "REDIR_OUT")
+		|| is_type_token(tokens[i], "REDIR_IN") || is_type_token(tokens[i], "APPEND")
+		|| is_type_token(tokens[i], "PIPE") || is_type_token(tokens[i], "HEREDOC"))
+		{
+			if (!is_valid_next_token(tokens, i)|| !is_valid_previous_token(tokens, i))
+				return (false);
+		}
+		i++;
+	}
+	return (true);
+}
